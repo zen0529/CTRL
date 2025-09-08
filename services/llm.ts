3e2457f7ada13df5d1d7cfdb7a9c6db972ca1071
aldren <aldren.arcenal.21@usjr.edu.ph>
2025-09-08 10:34:47 +0800

    added retries to prevent network errors

--- services/llm.ts
+++ services/llm.ts
@@ -2,27 +2,32 @@ import axios from "axios";
 import { userInput } from "../interfaces/interface";
 
 export default async function FetchRecommendedAction(user_input: userInput) {
-  try {
-    console.log("user_input", user_input);
-    
-    const recommended_action = await axios.post(
-      // "http://192.168.254.148:8000/Recommend_Actions", // for local
-      "https://ctrl-api-five.vercel.app/Recommend_Actions", //deployed on free hosting
-      user_input,
-      {
-        timeout: 10000, // add timeout to avoid crashing the app
+  const maxRetries = 2;
+  let attempt = 0;
+  while (attempt < maxRetries) {
+    try {
+      console.log("user_input", user_input);
+
+      const recommended_action = await axios.post(
+        // "http://192.168.254.148:8000/Recommend_Actions", // for local
+        "https://ctrl-api-five.vercel.app/Recommend_Actions", //deployed on free hosting
+        user_input,
+        {
+          timeout: 10000, // add timeout to avoid crashing the app
+        }
+      );
+
+      if (recommended_action.status !== 200) {
+        return "LLM Services Unavailable";
       }
-    );
-    
-     if (recommended_action.status !== 200) {
-      return "LLM Services Unavailable";
+      return recommended_action.data;
+    } catch (error) {
+      attempt++;
+      console.error("Request failed attempt number: ", attempt);
+
+      if (attempt >= maxRetries) {
+        throw error; 
+      }
     }
-    return recommended_action.data;
-  } catch (error) {
-    console.log("error")
-    // log internally
-    console.error("Request failed:", error);
-    // rethrow
-    throw error;
   }
 }
